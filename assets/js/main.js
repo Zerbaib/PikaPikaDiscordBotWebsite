475ccc8666718ee159c5ab3f3c9ad7f7f63b5b10
Zerbaib <81523999+Zerbaib@users.noreply.github.com>
2023-01-10 19:56:03 +0100

    test navbar Amaury

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+navResponsive = document.querySelector(".responsive")
+menu = document.getElementById("menu")
+
+menu.addEventListener("click", () => {
+        navResponsive.classList.toggle('hide')
+    
+})
